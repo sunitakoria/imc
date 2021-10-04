09dec7b0d2776933462e7c1253e0775e4b1836de
unknown <sunitakoria@gmail.com>
2021-10-04 12:36:58 +0530

    changes

---
+++ client/src/App.js
@@ -0,0 +1,141 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-restricted-globals */
+import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios';
+import { Button, Card } from 'react-bootstrap'
+class App extends Component {
+  constructor(props) {
+    super(props)
+this.state = {
+setBookName: '',
+setIn: '',
+setOut: '',
+fetchData: [],
+fetchAll:[],
+reviewUpdate: ''
+}
+  }
+
+  handleChange = (event) => {
+    let nam = event.target.name;
+    let val = event.target.value
+    this.setState({
+      [nam]: val
+    })
+  }
+
+  handleChange2 = (event) => {
+    this.setState({
+      reviewUpdate: event.target.value
+    })
+  }
+
+componentDidMount() {
+axios.get("http://localhost:3001/get")
+.then((response) => {
+this.setState({
+fetchData: response.data
+})
+})
+axios.get("http://localhost:3001/getAll")
+.then((response) => {
+this.setState({
+fetchAll: response.data
+})
+})
+}
+  submit = () => {
+    axios.post('http://localhost:3001/insert', this.state)
+      .then(() => { alert('success post') })
+    console.log(this.state)
+    document.location.reload();
+  }
+
+  delete = (id) => {
+    if (confirm("Do you want to delete? ")) {
+      axios.delete(`/api/delete/${id}`)
+      document.location.reload()
+    }
+  }
+
+  edit = (id) => {
+    axios.put(`/api/update/${id}`, this.state)
+    document.location.reload();
+  }
+  render() { return(
+
+    <>
+     <div className="d-flex justify-content-around">
+<Card style={{ width: '20rem' }}>
+<Card.Body>
+
+<div><h1>Entry</h1></div>
+<div className='form'>
+<input name='setBookName' placeholder='Enter Name' onChange={this.handleChange} />
+<input name='setIn' placeholder='Enter In' onChange={this.handleChange} />
+<input name='setOut' placeholder='Enter Out' onChange={this.handleChange} />
+
+
+</div>
+
+
+<Button className='my-2' variant="primary" onClick={this.submit}>Submit</Button> <br /><br />
+</Card.Body>
+</Card>
+<Card style={{ width: '40rem' }}>
+<Card.Body>
+
+<div><h1>Today sell/stock</h1></div>
+
+
+<table class="table" title="hello">
+<thead>
+<tr>
+<th scope="col">#</th>
+<th scope="col">Name</th>
+<th scope="col">Stock(In)</th>
+<th scope="col">Stock(Out)</th>
+</tr>
+</thead>
+<tbody>
+
+
+{this.state.fetchData.map((val, key) => { return (<><tr><th scope="row">{val.id}</th><td>{val.book_name}</td><td>{val.book_in}</td><td>{val.book_out}</td></tr></>); })}
+
+
+
+</tbody>
+</table>
+</Card.Body>
+</Card>
+
+
+<Card style={{ width: '40rem' }}>
+<Card.Body>
+<div><h1>Stock in Hand</h1></div>
+<table class="table">
+<thead>
+<tr>
+<th scope="col">#</th>
+<th scope="col">Name</th>
+<th scope="col">Stock(In)</th>
+<th scope="col">Stock(Out)</th>
+<th scope="col">available</th>
+</tr>
+</thead>
+<tbody>
+
+
+                {this.state.fetchAll.map((val, key) => { return (<><tr><th scope="row">{val.id}</th><td>{val.book_name}</td><td>{val.book_in}</td><td>{val.book_out}</td><td>{parseInt(val.book_in)-parseInt(val.book_out)}</td></tr></>); })}
+
+
+
+</tbody>
+</table>
+</Card.Body>
+</Card>
+</div></> );
+}
+}
+export default App;
---
+++ server/index.js
@@ -0,0 +1,133 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+var fs = require('fs');
+const path = require('path');
+
+// Add mysql database connection
+const dataJson = "./data.json";
+// Enable cors security headers
+app.use(cors())
+
+// add an express method to parse the POST method
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+// home page
+/*app.get('/', (req, res) => {
+  res.send('Hi There')
+});*/
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/', function (req, res) {
+   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+ });
+app.post("/insert", (req, res) => {
+  console.log('hhhhhh');
+  const bookName = req.body.setBookName;
+  const bookReview = req.body.setIn;
+  const setIn = req.body.setIn;
+  const setOut = req.body.setOut;
+  const dateBook =new Date().toISOString();
+const file = fs.readFileSync(dataJson);
+const dataRes = JSON.parse(file);
+if(!bookName){
+return false;
+}
+dataRes.push({id:dataRes.length,book_name:bookName,book_review:bookReview,book_in:setIn,book_out:setOut,book_date:dateBook
+});
+console.log('hhhhhh');
+
+fs.writeFileSync(dataJson,JSON.stringify(dataRes));
+});
+// add a book to the database
+
+app.get('/getAll', (req, res) => {
+
+const file = fs.readFileSync(dataJson);
+const dataRes = JSON.parse(file);
+let dataName = []
+let actualData = []
+dataRes.map((val)=>{
+if(!dataName.includes(val.book_name)){
+dataName.push(val.book_name);
+actualData.push(val);
+}else{
+const index = dataName.indexOf(val.book_name);
+const value = actualData[index];
+if(value.book_in==""){
+value.book_in=0;
+}
+if(value.book_out==""){
+value.book_out=0;
+}
+if(val.book_in==""){
+val.book_in=0;
+}
+if(val.book_out==""){
+val.book_out=0;
+}
+value.book_in=parseInt(value.book_in)+parseInt(val.book_in);
+value.book_out=parseInt(value.book_out)+parseInt(val.book_out);
+actualData[index] =value;
+}
+})
+res.send(actualData)
+})
+
+app.get('/get', (req, res) => {
+
+  const file = fs.readFileSync(dataJson);
+  const dataRes = JSON.parse(file);
+  let dataName = []
+  let actualData = []
+  const actualDate= new Date().toISOString();
+  dataRes.map((val)=>{
+  if(val.book_date.toString().includes(actualDate.substr(0,10))){
+  //dataName.push(val.book_name);
+  actualData.push(val);
+  }
+  })
+  res.send(actualData)
+  })
+  
+// add a book to the database
+app.post("/insert", (req, res) => {
+const bookName = req.body.setBookName;
+const bookReview = req.body.setIn;
+const setIn = req.body.setIn;
+const setOut = req.body.setOut;
+const dateBook =new Date().toISOString();
+const file = fs.readFileSync(dataJson);
+const dataRes = JSON.parse(file);
+if(!bookName){
+return false;
+}
+dataRes.push({id:dataRes.length,book_name:bookName,book_review:bookReview,book_in:setIn,book_out:setOut,book_date:dateBook
+});
+// Printing data
+console.log(dataRes)
+fs.writeFileSync('./data.json',JSON.stringify(dataRes));
+console.log('done')
+})
+// delete a book from the database
+app.delete("/delete/:bookId", (req, res) => {
+  const bookId = req.params.bookId;
+  const DeleteQuery = "DELETE FROM books_reviews WHERE id = ?";
+  db.query(DeleteQuery, bookId, (err, result) => {
+    if (err) console.log(err);
+  })
+})
+
+// update a book review
+app.put("/update/:bookId", (req, res) => {
+  const bookReview = req.body.reviewUpdate;
+  const bookId = req.params.bookId;
+  const UpdateQuery = "UPDATE books_reviews SET book_review = ? WHERE id = ?";
+  db.query(UpdateQuery, [bookReview, bookId], (err, result) => {
+    if (err) console.log(err)
+  })
+})
+
+app.listen('3001', () => { })
